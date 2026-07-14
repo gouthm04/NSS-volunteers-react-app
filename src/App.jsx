ab83a6c7a326990e30a895f9d12ac68d8b1f9b41
gouthm.04 <gouthm.04@gmail.com>
2026-07-14 22:46:24 +0530

    inialt react setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <h1>NSS Volunteer Management</h1>
+    </div>
+  );
+}
+
+export default App;
